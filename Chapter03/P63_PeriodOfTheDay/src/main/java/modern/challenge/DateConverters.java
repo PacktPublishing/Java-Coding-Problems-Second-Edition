9b19cb304cde0b61b70b00ee99fbc2d294d976bc
anghelleonard <leoprivacy@yahoo.com>
2023-02-03 12:38:29 +0200

    Working with Date and Time

--- Chapter03/P63_PeriodOfTheDay/src/main/java/modern/challenge/DateConverters.java
+++ Chapter03/P63_PeriodOfTheDay/src/main/java/modern/challenge/DateConverters.java
@@ -1,5 +1,7 @@
 package modern.challenge;
 
+import java.text.ParseException;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -12,18 +14,46 @@ public final class DateConverters {
         throw new AssertionError("Cannot be instantiated");
     }
 
-    public static String toDayPeriod(Date date, ZoneId zoneId) {
+    public static String toDayPeriodV1(Date date, ZoneId zoneId) throws ParseException {
+
+        Objects.requireNonNull(date, "The provided date cannot be null");
+        Objects.requireNonNull(zoneId, "The provided zone id cannot be null");
+
+        LocalTime lt = date.toInstant().atZone(zoneId).toLocalTime();
+       
+        LocalTime night = LocalTime.of(21, 0, 0);
+        LocalTime morning = LocalTime.of(6, 0, 0);
+        LocalTime afternoon = LocalTime.of(12, 0, 0);
+        LocalTime evening = LocalTime.of(18, 0, 0);
+        LocalTime almostMidnight = LocalTime.of(23, 59, 59);
+        LocalTime midnight = LocalTime.of(0, 0, 0);
+        
+        if((lt.isAfter(night) && lt.isBefore(almostMidnight)) 
+                || lt.isAfter(midnight) && (lt.isBefore(morning))) {
+            return "night";
+        } else if(lt.isAfter(morning) && lt.isBefore(afternoon)) {
+            return "morning";
+        } else if(lt.isAfter(afternoon) && lt.isBefore(evening)) {
+            return "afternoon";
+        } else if(lt.isAfter(evening) && lt.isBefore(night)) {
+            return "evening";
+        }
+
+        return "day";
+    }
+
+    public static String toDayPeriodV2(Date date, ZoneId zoneId) {
 
         Objects.requireNonNull(date, "The provided date cannot be null");
         Objects.requireNonNull(zoneId, "The provided zone id cannot be null");
 
         ZonedDateTime zdt = date.toInstant().atZone(zoneId);
 
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MMM-dd'T'HH:mm:ss a Z");        
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MMM-dd'T'HH:mm:ss a Z");
 
         System.out.println("Date-time to format as day period is: "
                 + zdt.withZoneSameInstant(zoneId).format(formatter1));
-        
+
         DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MMM-dd [B]");
 
         return zdt.withZoneSameInstant(zoneId).format(formatter2);
--- Chapter03/P63_PeriodOfTheDay/src/main/java/modern/challenge/Main.java
+++ Chapter03/P63_PeriodOfTheDay/src/main/java/modern/challenge/Main.java
@@ -1,30 +1,58 @@
 package modern.challenge;
 
+import java.text.ParseException;
 import java.time.ZoneId;
 import java.util.Date;
 
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws ParseException {
+
         System.out.println(ZoneId.getAvailableZoneIds());
-        
+
         System.out.println();
-        
+
         // Africa/Johannesburg
-        System.out.println("Africa/Johannesburg: " 
-                + DateConverters.toDayPeriod(new Date(), ZoneId.of("Africa/Johannesburg")));
-        
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("Africa/Johannesburg"))
+                + ", Africa/Johannesburg");
+        System.out.println("Africa/Johannesburg: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("Africa/Johannesburg")));
+
         System.out.println();
-        
+
         // Asia/Saigon
-        System.out.println("Asia/Saigon: " 
-                + DateConverters.toDayPeriod(new Date(), ZoneId.of("Asia/Saigon")));
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("Asia/Saigon"))
+                + ", Asia/Saigon");
+        System.out.println("Asia/Saigon: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("Asia/Saigon")));
+
+        System.out.println();
+
+        // Australia/Melbourne
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("Australia/Melbourne"))
+                + ", Australia/Melbourne");
+        System.out.println("Australia/Melbourne: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("Australia/Melbourne")));
+
+        System.out.println();
+
+        // America/Resolute
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("America/Resolute"))
+                + ", America/Resolute");
+        System.out.println("America/Resolute: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("America/Resolute")));
         
         System.out.println();
         
-        // America/Resolute
-        System.out.println("America/Resolute: " 
-                + DateConverters.toDayPeriod(new Date(), ZoneId.of("America/Resolute")));
+        //  America/Argentina/Buenos_Aires
+        System.out.println("Good "
+                + DateConverters.toDayPeriodV1(new Date(), ZoneId.of("America/Argentina/Buenos_Aires"))
+                + ", America/Argentina/Buenos_Aires");
+        System.out.println("America/Argentina/Buenos_Aires: "
+                + DateConverters.toDayPeriodV2(new Date(), ZoneId.of("America/Argentina/Buenos_Aires")));
     }
 }
