e003960072e7a34f7ea35793239e29800aee44dc
anghelleonard <leoprivacy@yahoo.com>
2023-02-06 17:54:20 +0200

    Working with Date and Time

--- Chapter03/P75_SystemUTCVsSystemDefaultZone/src/main/java/modern/challenge/Main.java
+++ Chapter03/P75_SystemUTCVsSystemDefaultZone/src/main/java/modern/challenge/Main.java
@@ -1,23 +1,28 @@
 package modern.challenge;
 
 import java.time.Clock;
+import static java.time.Clock.system;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(Clock.systemDefaultZone());
+        System.out.println(Clock.systemDefaultZone());       
+        System.out.println(system(ZoneId.systemDefault()));        
         System.out.println(Clock.systemUTC());
         
         System.out.println();
         
         System.out.println(Clock.systemDefaultZone().instant());
+        System.out.println(system(ZoneId.systemDefault()).instant());        
         System.out.println(Clock.systemUTC().instant());
         
         System.out.println();
         
         System.out.println(LocalDateTime.now(Clock.systemUTC()));
         System.out.println(LocalDateTime.now(Clock.systemDefaultZone()));       
+        System.out.println(LocalDateTime.now(system(ZoneId.systemDefault())));       
     }
 }
