9df075450498c7a119a2f30934b71bb1491b8839
anghelleonard <leoprivacy@yahoo.com>
2023-02-09 14:41:33 +0200

    Working with Date and Time

---
+++ Chapter04/P85_RecordCanonicalCntr/src/main/java/modern/challenge/MarketRecord.java
@@ -0,0 +1,10 @@
+package modern.challenge;
+
+import java.util.Map;
+
+public record MarketRecord(Map<String, Integer> retailPrices) {
+
+    public MarketRecord {
+        retailPrices = Map.copyOf(retailPrices);
+    }
+}
