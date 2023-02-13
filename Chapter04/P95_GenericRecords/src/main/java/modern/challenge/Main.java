4258c328c77d451dac511a7fe2881d43e3c60cb6
anghelleonard <leoprivacy@yahoo.com>
2023-02-13 17:17:56 +0200

    Record and record pattern

--- Chapter04/P95_GenericRecords/src/main/java/modern/challenge/Main.java
+++ Chapter04/P95_GenericRecords/src/main/java/modern/challenge/Main.java
@@ -7,15 +7,24 @@ public class Main {
        FruitRecord<MelonRecord> fruit = 
                new FruitRecord<>(new MelonRecord("Hami", 1000), "China");
        
+       if (fruit instanceof FruitRecord<MelonRecord>(MelonRecord melon, String country)) {
+          System.out.println(melon + " from " + country);
+       }
+       
        if (fruit instanceof FruitRecord<MelonRecord>(var melon, var country)) {
           System.out.println(melon + " from " + country);
        }
        
        switch(fruit) {
+           case FruitRecord<MelonRecord>(MelonRecord melon, String country) : 
+               System.out.println(melon + " from " + country); break;
+           default : break;           
+       };
+       
+       switch(fruit) {
            case FruitRecord<MelonRecord>(var melon, var country) : 
                System.out.println(melon + " from " + country); break;
-           default : break;
-           
+           default : break;           
        };
     }
 }
