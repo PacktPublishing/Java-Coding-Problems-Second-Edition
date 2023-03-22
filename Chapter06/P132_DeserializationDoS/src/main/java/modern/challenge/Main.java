58e5558aaa63ccfe92f70cb863d523650c09226a
anghelleonard <leoprivacy@yahoo.com>
2023-03-22 15:16:35 +0200

    Java I/O: Context-Specific Deserialization Filters

---
+++ Chapter06/P132_DeserializationDoS/src/main/java/modern/challenge/Main.java
@@ -0,0 +1,47 @@
+package modern.challenge;
+
+import java.io.IOException;
+import java.io.ObjectInputFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        ArrayList<Object> startList = new ArrayList<>();
+        
+        List<Object> list1 = startList;
+        List<Object> list2 = new ArrayList<>();
+        
+        for (int i = 0; i < 101; i++) {
+            
+            List<Object> sublist1 = new ArrayList<>();
+            List<Object> sublist2 = new ArrayList<>();
+            
+            sublist1.add("value: " + i);
+
+            list1.add(sublist1);
+            list1.add(sublist2);
+
+            list2.add(sublist1);
+            list2.add(sublist2);
+
+            list1 = sublist1;
+            list2 = sublist2;
+        }
+             
+        // create a filter to prvent DoS
+        ObjectInputFilter filter = ObjectInputFilter.
+                Config.createFilter("maxdepth=10;java.base/*;!*");
+        
+        byte[] mapSer = Converters.objectToBytes(startList);        
+        System.out.println("Serialization: " + Arrays.toString(mapSer));
+        
+        System.out.println();
+                
+        ArrayList mapDeser = (ArrayList) Converters.bytesToObject(mapSer, filter);        
+        System.out.println("Deserialization: " + mapDeser);        
+    }
+}
