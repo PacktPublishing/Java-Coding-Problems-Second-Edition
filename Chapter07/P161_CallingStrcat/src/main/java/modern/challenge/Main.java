4e4c09628835cdf290bd0f50186b98c95364b689
anghelleonard <leoprivacy@yahoo.com>
2023-04-10 12:03:35 +0300

    Foreign (Function) Memory API

--- Chapter07/P161_CallingStrcat/src/main/java/modern/challenge/Main.java
+++ Chapter07/P161_CallingStrcat/src/main/java/modern/challenge/Main.java
@@ -12,28 +12,29 @@ public class Main {
 
     public static void main(String[] args) throws Throwable {
 
-        String dest = "Hello ";
-        String src = "World";
+        String strDestination = "Hello ";
+        String strSource = "World";
 
         Linker linker = Linker.nativeLinker();
-        SymbolLookup mathLookup = linker.defaultLookup();
+        SymbolLookup libLookup = linker.defaultLookup();
 
         try (Arena arena = Arena.openConfined()) {
 
-            MemorySegment segmentMath = mathLookup.find("strcat").get();
+            MemorySegment segmentStrcat = libLookup.find("strcat").get();
 
-            MethodHandle func = linker.downcallHandle(segmentMath, FunctionDescriptor.ofVoid(
+            MethodHandle func = linker.downcallHandle(segmentStrcat, FunctionDescriptor.ofVoid(
                     ValueLayout.ADDRESS, ValueLayout.ADDRESS));
 
-            MemorySegment segmentSrc = arena.allocate(src.length() + 1);
-            segmentSrc.setUtf8String(0, src);
+            MemorySegment segmentStrSource = arena.allocate(strSource.length() + 1);
+            segmentStrSource.setUtf8String(0, strSource);
 
-            MemorySegment segmentDest = arena.allocate(src.length() + 1 + dest.length() + 1);
-            segmentDest.setUtf8String(0, dest);
+            MemorySegment segmentStrDestination = arena.allocate(
+                    strSource.length() + 1 + strDestination.length() + 1);
+            segmentStrDestination.setUtf8String(0, strDestination);
 
-            func.invokeExact(segmentDest, segmentSrc);
+            func.invokeExact(segmentStrDestination, segmentStrSource);
 
-            System.out.println("Result: " + segmentDest.getUtf8String(0));
+            System.out.println(segmentStrDestination.getUtf8String(0));
         }
     }
 }
