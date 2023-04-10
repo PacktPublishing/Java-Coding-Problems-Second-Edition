8513afb0186ac196fd8433c71e5e32c7a311f5a7
anghelleonard <leoprivacy@yahoo.com>
2023-04-10 10:40:53 +0300

    Foreign (Function) Memory API

--- Chapter07/P158_CallingGetpid/src/main/java/modern/challenge/Main.java
+++ Chapter07/P158_CallingGetpid/src/main/java/modern/challenge/Main.java
@@ -1,29 +1,32 @@
 package modern.challenge;
 
-import java.lang.foreign.Arena;
 import java.lang.foreign.FunctionDescriptor;
 import java.lang.foreign.Linker;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.SymbolLookup;
 import java.lang.foreign.ValueLayout;
 import java.lang.invoke.MethodHandle;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) throws Throwable {
 
+        // get the Linker of the underlying native platform 
+        // (operating system + processor that runs the JVM)
         Linker linker = Linker.nativeLinker();
-
+        
+        // "_getpid" is part of the Universal C Runtime (UCRT) Library
         SymbolLookup mathLookup = linker.defaultLookup();
+        
+        // find the "_getpid" foreign function
         MemorySegment segmentMath = mathLookup.find("_getpid").get();
 
+        // create a method handle for "_getpid"
         MethodHandle func = linker.downcallHandle(segmentMath,
                 FunctionDescriptor.of(ValueLayout.JAVA_INT));
 
-        int result = (int) func.invokeExact();
-
+        // invoke the foreign function, "_getpid" and get the result
+        int result = (int) func.invokeExact();       
         System.out.println(result);
     }
 }
