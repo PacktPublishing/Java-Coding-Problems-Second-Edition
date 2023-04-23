cb7bf9fdc5355bf14d43c7a3a0506d91c96178a3
anghelleonard <leoprivacy@yahoo.com>
2023-04-23 18:01:04 +0300

    Sealed and Hidden Classes

--- Chapter08/P173_SealedClassesAndVisitorPattern2/src/main/java/modern/challenge/ElectricComponentVisitor.java
+++
@@ -1,9 +0,0 @@
-package modern.challenge;
-
-public interface ElectricComponentVisitor {
-    
-    String visit(Capacitor capacitor);
-    String visit(Transistor transistor);
-    String visit(Resistor resistor);
-    String visit(ElectricCircuit circuit);
-}
