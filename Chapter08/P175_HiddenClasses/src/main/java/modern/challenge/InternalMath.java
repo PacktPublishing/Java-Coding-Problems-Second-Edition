9b350ad740a2132b3266d87361150b6ac11784f0
anghelleonard <leoprivacy@yahoo.com>
2023-04-25 11:29:07 +0300

    Sealed and Hidden Classes

--- Chapter08/P175_HiddenClasses/src/main/java/modern/challenge/InternalMath.java
+++ Chapter08/P175_HiddenClasses/src/main/java/modern/challenge/InternalMath.java
@@ -2,7 +2,7 @@ package modern.challenge;
 
 import java.util.stream.IntStream;
 
-public class InternalMath {
+public class InternalMath implements Math {
     
     public long sum(int[] nr) {
         return IntStream.of(nr).sum();
--- Chapter08/P175_HiddenClasses/src/main/java/modern/challenge/Main.java
+++ Chapter08/P175_HiddenClasses/src/main/java/modern/challenge/Main.java
@@ -9,29 +9,30 @@ import java.lang.reflect.Method;
 
 public class Main {
 
-    public static void main(String[] args) 
-            throws IOException, IllegalAccessException, NoSuchMethodException, 
+    public static void main(String[] args)
+            throws IOException, IllegalAccessException, NoSuchMethodException,
             InstantiationException, IllegalArgumentException, InvocationTargetException {
 
         /* creating a hidden class */
         
         MethodHandles.Lookup lookup = MethodHandles.lookup();
 
-        Class<?> clazz = InternalMath.class;        
+        Class<?> clazz = InternalMath.class;
         String clazzPath = clazz.getName().replace('.', '/') + ".class";
-        InputStream stream = clazz.getClassLoader().getResourceAsStream(clazzPath);        
+        InputStream stream = clazz.getClassLoader().getResourceAsStream(clazzPath);
         byte[] clazzBytes = stream.readAllBytes();
-        
+
         Class<?> hiddenClass = lookup.defineHiddenClass(clazzBytes,
                 true, ClassOption.NESTMATE).lookupClass();
-        
+
         /* using a hidden class */
         
-        Object obj = hiddenClass.getConstructor().newInstance();
-        
+        // Object obj = hiddenClass.getConstructor().newInstance();   // without the Math interface
+        Math obj = (Math) hiddenClass.getConstructor().newInstance(); // with the Math interface
+
         Method method = obj.getClass()
                 .getDeclaredMethod("sum", int[].class);
-        
-        System.out.println(method.invoke(obj, new int[] {4, 1, 6, 7}));
+
+        System.out.println(method.invoke(obj, new int[]{4, 1, 6, 7}));
     }
 }
