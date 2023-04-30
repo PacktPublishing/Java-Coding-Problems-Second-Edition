037bce12eb767ada99d9a273b301b0af6043a86d
anghelleonard <leoprivacy@yahoo.com>
2023-04-30 13:50:54 +0300

    Functional style programming - extending API

---
+++ Chapter09/P181_FilterNestedCollection/src/main/java/modern/challenge/Author.java
@@ -0,0 +1,54 @@
+package modern.challenge;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Author {
+
+    private final String name;
+    private final List<Book> books;
+
+    public Author(String name, List<Book> books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + Objects.hashCode(this.books);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Author other = (Author) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return Objects.equals(this.books, other.books);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "name=" + name + ", books=" + books + '}';
+    }   
+}
