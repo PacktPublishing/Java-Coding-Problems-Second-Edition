7076f78ec70a87cbb86c5405f38515c0d1f885b5
anghelleonard <leoprivacy@yahoo.com>
2023-05-10 10:04:23 +0300

    Functional style programming - extending API

--- Chapter09/P179_StreamMapCustomMap/src/main/java/modern/challenge/Main.java
+++ Chapter09/P179_StreamMapCustomMap/src/main/java/modern/challenge/Main.java
@@ -14,18 +14,29 @@ public class Main {
 
         List<Post> posts = List.of(
                 new Post(1, "Running jOOQ", "#database #sql #rdbms"),
-                new Post(2, "Reading/writing files in Java", "#io #storage #rdbms"),
-                new Post(3, "Working with Hibernate", "#jpa #database #rdbms"),
+                new Post(2, "I/O files in Java", "#io #storage #rdbms"),
+                new Post(3, "Hibernate Course", "#jpa #database #rdbms"),
                 new Post(4, "Hooking Java Sockets", "#io #network"),
                 new Post(5, "Analysing JDBC transactions", "#jdbc #rdbms")
         );
 
         Map<String, List<Integer>> result = posts.stream()
                 .flatMap(post -> Post.allTags(post).stream()
-                        .map(p -> entry(p, post.getId())))
-                .collect(groupingBy(Entry::getKey, 
+                .map(t -> entry(t, post.getId())))
+                .collect(groupingBy(Entry::getKey,
                         mapping(Entry::getValue, toList())));
 
-        System.out.println(result);               
+        System.out.println(result);
+
+        Map<String, List<Integer>> resultMulti = posts.stream()
+                .<Map.Entry<String, Integer>>mapMulti((post, consumer) -> {
+                    for (String tag : Post.allTags(post)) {
+                        consumer.accept(entry(tag, post.getId()));
+                    }
+                })
+                .collect(groupingBy(Entry::getKey,
+                        mapping(Entry::getValue, toList())));
+
+        System.out.println(resultMulti);
     }
 }
