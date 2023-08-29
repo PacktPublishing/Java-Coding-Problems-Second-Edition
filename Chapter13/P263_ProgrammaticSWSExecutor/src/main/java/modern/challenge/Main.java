930b9c68da7bf8fde9f346a78df9c90d6ac1d067
anghelleonard <leoprivacy@yahoo.com>
2023-08-29 10:23:24 +0300

    Socket API and Simple Web Server

---
+++ Chapter13/P263_ProgrammaticSWSExecutor/src/main/java/modern/challenge/Main.java
@@ -0,0 +1,23 @@
+package modern.challenge;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.SimpleFileServer;
+import com.sun.net.httpserver.SimpleFileServer.OutputLevel;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HttpServer sws = SimpleFileServer.createFileServer(
+                new InetSocketAddress(9009),
+                Path.of("C:\\SBPBP\\GitHub\\Java-Coding-Problems-Second-Edition\\Chapter13\\P263_ProgrammaticSWS\\docs"),
+                OutputLevel.VERBOSE);
+        
+        sws.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        
+        sws.start();
+    }
+}
