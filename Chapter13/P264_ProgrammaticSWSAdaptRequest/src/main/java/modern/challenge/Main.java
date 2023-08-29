427b90b27946f0f62f2394c2814c1e0684153ecc
anghelleonard <leoprivacy@yahoo.com>
2023-08-29 17:21:20 +0300

    Socket API and Simple Web Server

---
+++ Chapter13/P264_ProgrammaticSWSAdaptRequest/src/main/java/modern/challenge/Main.java
@@ -0,0 +1,29 @@
+package modern.challenge;
+
+import com.sun.net.httpserver.Filter;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.SimpleFileServer;
+import java.io.IOException;
+import static java.lang.System.out;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+
+        HttpHandler fileHandler = SimpleFileServer.createFileHandler(
+                Path.of("C:\\SBPBP\\GitHub\\Java-Coding-Problems-Second-Edition\\Chapter13\\P264_ProgrammaticSWSAdaptRequest\\docs"));
+                        
+        Filter preFilter = Filter.adaptRequest(
+                "Add 'Author' header", r -> r.with("Author", List.of("Anghel Leonard")));
+        Filter postFilter = SimpleFileServer.createOutputFilter(out, SimpleFileServer.OutputLevel.VERBOSE);
+        
+        HttpServer sws = HttpServer.create(
+                new InetSocketAddress(9009), 10, "/mybooks", fileHandler, preFilter, postFilter);                                
+        
+        sws.start();
+    }
+}
---
+++ Chapter13/P265_ProgrammaticSWSComplementHandler/src/main/java/modern/challenge/Main.java
@@ -0,0 +1,33 @@
+package modern.challenge;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpHandlers;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.Request;
+import com.sun.net.httpserver.SimpleFileServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+
+        HttpHandler complementHandler = HttpHandlers.of(200, Headers.of(
+                "Content-Type", "text/plain"), "No data available");
+        
+        HttpHandler fileHandler = SimpleFileServer.createFileHandler(
+                Path.of("C:\\SBPBP\\GitHub\\Java-Coding-Problems-Second-Edition\\Chapter13\\P265_ProgrammaticSWSComplementHandler\\docs"));                    
+        
+        Predicate<Request> predicate = request -> request.getRequestMethod().equalsIgnoreCase("GET");
+        
+        HttpHandler handler = HttpHandlers.handleOrElse(predicate, fileHandler, complementHandler);
+                        
+        HttpServer sws = HttpServer.create(
+                new InetSocketAddress(9009), 10, "/mybooks", handler);                                
+        
+        sws.start();
+    }
+}
