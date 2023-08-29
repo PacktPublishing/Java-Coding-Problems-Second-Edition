d48bbdc2a2d210399bb50467362919bf4c8c4f81
anghelleonard <leoprivacy@yahoo.com>
2023-08-29 18:37:51 +0300

    Socket API and Simple Web Server

---
+++ Chapter13/P266_SWSInMemoryFileSystem/src/main/java/modern/challenge/Main.java
@@ -0,0 +1,45 @@
+package modern.challenge;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.SimpleFileServer;
+import com.sun.net.httpserver.SimpleFileServer.OutputLevel;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer sws = SimpleFileServer.createFileServer(
+                new InetSocketAddress(9009),
+                inMemoryDirectory(),
+                OutputLevel.VERBOSE);
+
+        sws.start();
+    }
+
+    private static Path inMemoryDirectory() throws IOException {
+
+        FileSystem fileSystem = Jimfs.newFileSystem(Configuration.unix());
+
+        Path docs = fileSystem.getPath("/docs");
+        Files.createDirectory(docs);
+
+        Path books = docs.resolve("books.txt"); // /docs/books.txt
+        Files.write(books, ImmutableList.of(
+                "Java Coding Problems 1st Edition",
+                "Java Coding Problems 2st Edition",
+                "jOOQ Masterclass",
+                "The Complete Coding Interview Guide in Java"),
+                StandardCharsets.UTF_8);
+
+        return docs;
+    }
+}
