0aa7810a9186319ea04fe06d323f3b8ee48b7c1b
anghelleonard <leoprivacy@yahoo.com>
2024-04-11 16:43:11 +0300

    Foreign (Function) Memory API

---
+++ Chapter07/JDK 22/P164_MappedMemorySegment/README.md
@@ -0,0 +1,4 @@
+# Tackling mapped memory segments:
+Provide a brief introduction of mapped memory segments and exemplify them in Java code.
+
+## Using JDK 21
---
+++ Chapter07/JDK 22/P164_MappedMemorySegment/src/main/java/modern/challenge/Main.java
@@ -0,0 +1,55 @@
+package modern.challenge;
+
+import java.io.IOException;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.nio.channels.FileChannel;
+import static java.nio.channels.FileChannel.MapMode.READ_WRITE;
+import java.nio.file.Path;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.SPARSE;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        try (FileChannel file = FileChannel.open(
+                Path.of("readme.txt"), CREATE, READ, WRITE);                 
+                Arena arena = Arena.ofConfined()) {
+
+            MemorySegment segment
+                    = file.map(READ_WRITE, 0, 1048576, arena);  // 1 Megabyte = 1048576 bytes
+                                                                // 1 Gigabyte = 1073741824 bytes
+
+            // write the data                                                                        
+            segment.setString(0, "This is a readme file ...");
+            segment.setString(1048576/2, "Here is the middle of the file ...");
+            segment.setString(1048576-32, "Here is the end of the file ...");
+            
+            // read some data
+            System.out.println(segment.getString(1048576/2));
+        }
+        
+        try (FileChannel file = FileChannel.open(
+                Path.of("sparse_readme.txt"), CREATE_NEW, SPARSE, READ, WRITE);                 
+                Arena arena = Arena.ofConfined()) {
+
+            MemorySegment segment
+                    = file.map(READ_WRITE, 0, 1048576, arena);  // 1 Megabyte = 1048576 bytes
+                                                                // 1 Gigabyte = 1073741824 bytes
+
+            // write the data                                                                        
+            segment.setString(0, "This is a readme file ...");
+            segment.setString(1048576/2, "Here is the middle of the file ...");
+            segment.setString(1048576-32, "Here is the end of the file ...");
+            
+            // read some data
+            System.out.println(segment.getString(0));
+        }
+        
+        System.out.println("\n\nYou should find the files on disk in the project's root");
+    }
+}
